ccf8e8155cc80233e86ea80bc5e6f426eeb4a2f0
AustinMegier <60328872+AustinMegier@users.noreply.github.com>
2023-05-26 17:33:26 +1000

    completed exercises 9-12

---
+++ ex9.py
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# Assign a string literal to a variable called days
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Assign a string literal to a variable called months
+# Each month is on a new line because of the \n escape character
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# Print a multi-line string enclosed by triple quotes
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
