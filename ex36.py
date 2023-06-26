220f79b8ef7bb6f4ce2a40bb04da1efcb1a106a1
AustinMegier <60328872+AustinMegier@users.noreply.github.com>
2023-06-26 10:51:23 +1000

    begun ex36

---
+++ ex36.py
@@ -0,0 +1,35 @@
+# This exercise is to create a text game similar to the last exercise.
+# The premise: Traversing a plain of obstacles to achieve freedom.
+
+# Rooms:
+    # The Nest (unit)
+    # The Open Night ('dark')
+    # The Open Day ('light')
+    # The Airport (disguised freedom)
+    # The Terminal (illusion of being almost there)
+    # The Plane (a bitter taste of freedom)
+    # The Destination (false freedom)
+    # Contentment (real freedom)
+
+
+# Monsters:
+# The Nest
+    # The Big Scary Nothing (initial hesitation)
+# The Open Night
+    # Silhouette (the unknown)
+# The Airport
+    # A Crowd of Businessmen (the pressure)
+# The Terminal
+    # The Intimidating Squirrel (last minute pull out)
+
+
+# Traps:
+# The Open Day
+    # Café (the known)
+# The Airport
+    # Did you bring your passport? (commitment)
+# The Plane
+    # Drinks multiple (5 or more and you're stuck?)
+# The Destination
+    # The feeling of "I did it!" (illusion, sends back to the beginning)
+
