ded0b0f3bf34e4d51cb8e8a6b294cacc51aa5455
AustinMegier <60328872+AustinMegier@users.noreply.github.com>
2023-09-26 15:53:00 +1000

    begun exercise 45

---
+++ ex45.py
@@ -0,0 +1,7 @@
+from sys import exit
+# import a module that YOU make as it is a requirement of the exercise. I'm not yet sure what to have in this module (e.g., classes with methods or just functions?).
+
+# Map Class that "knows" about the rooms. One way to do this is how ZS did in ex43 (with a list), but there should be many ways to do it)
+
+
+# Engine Class. Like the Map Class, try and do this in a different way to how ZS did it.
